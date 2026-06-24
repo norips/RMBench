fedeede606716ec5819e21f1439a6014e017c615
Raphael Druon <raphdruon@gmail.com>
2026-06-24 15:24:41 +0900

    Fix mem0 deps

--- script/_download_mem0_assets.sh
+++ script/_download_mem0_assets.sh
@@ -2,3 +2,7 @@
 
 huggingface-cli download Qwen/Qwen3-VL-2B-Instruct --local-dir policy/Mem-0/checkpoints/Qwen3-VL-2B-Instruct
 huggingface-cli download qiuly/Mem-0-m1mix-RMBench --local-dir policy/Mem-0/checkpoints/Mem-0-m1mix-RMBench
+
+cd policy/Mem-0/checkpoints/Mem-0-m1mix-RMBench/checkpoint/
+cat m1_mix_final_step50000.pt.part?? > m1_mix_final_step50000.pt
+sha256sum -c m1_mix_final_step50000.pt.sha256
